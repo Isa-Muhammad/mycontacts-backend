d5c65161c742a9966bb69bf5826f7e8a48eb6a4e
Isa-Muhammad <36093501+Isa-Muhammad@users.noreply.github.com>
2024-05-15 07:53:26 +0100

    Protect update and delete routes

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -53,6 +53,13 @@ const updateContact = asyncHandler(async (req, res) => {
     throw new Error("Contact not found");
   }
 
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error(
+      "User does not have permission to update other users contacts"
+    );
+  }
+
   const updatedContact = await Contact.findByIdAndUpdate(
     req.params.id,
     req.body,
@@ -70,7 +77,13 @@ const deleteContact = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Contact not found");
   }
-  await Contact.deleteOne();
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error(
+      "User does not have permission to update other users contacts"
+    );
+  }
+  await Contact.deleteOne({ _id: req.params.id });
 
   res.status(200).json(contact);
 });
