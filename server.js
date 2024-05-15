d30667d62c6114754e1b110b23267bb6e9626b28
Isa-Muhammad <36093501+Isa-Muhammad@users.noreply.github.com>
2024-05-15 02:32:48 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
+const dotenv = require("dotenv").config();
+
+connectDb();
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// This will provide a parser to parse data stream received from the client
+app.use(express.json());
+
+// Adding middleware to express project
+app.use("/api/contacts", require("./routes/contactRoutes"));
+
+// Use custom error handler
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
